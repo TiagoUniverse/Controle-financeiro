6d04cc9e442051e284fe87ac5fb72bc057139bac
TiagoUniverse <tiagocesar68@gmail.com>
2023-02-01 15:03:56 -0300

    Tela de mes atualizada com validação

--- Src/View/mes.php
+++ Src/View/mes.php
@@ -31,19 +31,26 @@ $ano = $_POST['ano'];
 <?php
  if ($nomeMes == "" || $ano == ""){
   ?>
-    <!-- <h5>Por favor, selecione um ano correto.</h5> -->
-  <?php
- }
-?>
-
-
-
-<div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="../../Assets/img/error.png" alt="" width="72" height="70">
     
-    <h1 class="display-5 fw-bold">Formulário</h1>
+    <h1 class="display-5 fw-bold">Error de ano</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <p class="lead mb-4">Por favor, digite um ano e mês válido na tela inicial.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <a href="home.php" class="btn btn-primary btn-lg px-4 gap-3" > Voltar</a>
+      </div>
+    </div>
+  </div>
+  <?php
+ } else {
+  ?>
+  <div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="../../Assets/img/dia 15.png" alt="" width="72" height="70">
+    
+    <h1 class="display-5 fw-bold">Primeira quinzena</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Registre os gastos entre o dia 01 até o dia 19, que são os dias da entrada de salário da casa.</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
         <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
         <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
@@ -52,16 +59,23 @@ $ano = $_POST['ano'];
   </div>
 
   <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="display-5 fw-bold">Formulário</h1>
+    <img class="d-block mx-auto mb-4" src="../../Assets/img/dia 30.png" alt="" width="72" height="70">
+    <h1 class="display-5 fw-bold">Segunda quinzena</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <p class="lead mb-4">Registre os gastos entre o dia 20 até dia 04 do próximo mês, antes de começar uma nova quinzena.</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
         <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
         <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
       </div>
     </div>
   </div>
+  <?php
+ }
+?>
+
+
+
+
 
 
 
