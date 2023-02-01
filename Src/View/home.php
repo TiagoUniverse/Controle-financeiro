18b458753f9de09f856f8a68e84c1b85a6d5d6cb
TiagoUniverse <tiagocesar68@gmail.com>
2023-02-01 16:57:11 -0300

    Validação na home

--- Src/View/home.php
+++ Src/View/home.php
@@ -40,7 +40,7 @@ if (isset($_POST['ano']) != null) {
   </form>
 
   <form method="POST" action="home.php">
-    <button type="submit" class="btn btn-secondary limparFiltro" style="margin-left: 0.3cm;">Limpar filtro</button>
+    <button type="submit" class="btn btn-secondary limparFiltro" style="margin-left: 0.3cm;">Limpar resultado</button>
   </form>
   </div>
 
@@ -174,6 +174,19 @@ if (isset($_POST['ano']) != null) {
       </div>
     </div>
   <?php
+  } else {
+    ?>
+    <div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="../../Assets/img/calendario.png" alt="" width="72" height="70">
+    
+    <h1 class="display-5 fw-bold">Busca vazia</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Digite um ano para começar</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      </div>
+    </div>
+  </div>
+    <?php
   }
   ?>
 
