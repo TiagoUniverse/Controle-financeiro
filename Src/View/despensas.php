0908c681e17802f640d69155ebabdc9e995f3f3b
TiagoUniverse <tiagocesar68@gmail.com>
2023-02-16 14:16:10 -0300

    Validação no registro de entradas

--- Src/View/despensas.php
+++ Src/View/despensas.php
@@ -80,15 +80,25 @@ if ($adicionando_registro != null && $adicionando_registro == "SALVANDO REGISTRO
   * │                                Validações                                                                     │
   * └───────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
   */
-  echo $_POST['data'];
-  $dataDividida = explode("-", $_POST['data']);
-  echo "<br>";
-  var_dump($dataDividida);
 
-  echo "<br>";
-  $mes_selecionado = "0" . $_SESSION['statusMes'];
-  echo $mes_selecionado;
+  if (isset($_POST['data'])) {
+    $dataDividida = explode("-", $_POST['data']);
+  }
 
+
+  // Mês para validação
+  if ($_SESSION['statusMes'] < 10) {
+    $mes_selecionado = "0" . $_SESSION['statusMes'];
+  } else {
+    $mes_selecionado = $_SESSION['statusMes'];
+  }
+
+  /**
+   * Mensagem Vermelha
+   * Funcionamento: A validação começa com ela definida como verdadeira. Se depois de todas as validações as informações estiverem corretas, então o sistema
+   * vai exibir uma mensagem de sucesso e informar que o cadastro foi feito com sucesso
+   * Data: 16/02/23
+   */
 
   $mensagemVermelha = true;
   if (!isset($_POST['data'])) {
@@ -100,7 +110,7 @@ if ($adicionando_registro != null && $adicionando_registro == "SALVANDO REGISTRO
   } else if ($descricao == null) {
     $mensagem = "Por favor, preencha a descrição sobre o registro";
   } else if ($valor <= 0) {
-    $mensagem = "Por favor, informe um valor positivo";
+    $mensagem = "Por favor, informe um valor positivo do dinheiro";
   } else {
 
     $retorno = $Despensas_repositorio->consultarRegistro($descricao, $valor, $data, $pdo);
@@ -117,97 +127,112 @@ if ($adicionando_registro != null && $adicionando_registro == "SALVANDO REGISTRO
   }
 
   $adicionando_registro = null;
-}
+
+  // Mensagem do resultado
+  if ($mensagemVermelha) {
+  ?>
+    <div class="alert alert-danger" role="alert">
+  <?php
+  } else {
+  ?>
+    <div class="alert alert-success" role="alert">
+  <?php
+  }
+    echo $mensagem;
+  ?>
+    </div>
+  <?php
+  }
 
 
-$consulta = $pdo->query("Select id, descricao, valor, DATE_FORMAT(dataDespensa, '%d/%m/%Y') as dataDespensa, ano, quinzena from despensas where status = 'ATIVO' 
+  $consulta = $pdo->query("Select id, descricao, valor, DATE_FORMAT(dataDespensa, '%d/%m/%Y') as dataDespensa, ano, quinzena from despensas where status = 'ATIVO' 
 and ano = '{$_SESSION['ano']}'  and IdStatus_mes = '{$_SESSION['statusMes']}' and quinzena = '{$_SESSION['quinzena']}' 
 and ( idStatus_despensa = 3 OR idstatus_despensa = 4 )          ");
 
-?>
+    ?>
 
-<!doctype html>
-<html lang="en">
+    <!doctype html>
+    <html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title> Despensas de <?php echo $_SESSION['nomeMes']; ?> de <?php echo $_SESSION['ano']; ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../Assets/Css/Content.css" />
-  <link rel="stylesheet" href="../../Assets/Css/Footer.css" />
-</head>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title> Despensas de <?php echo $_SESSION['nomeMes']; ?> de <?php echo $_SESSION['ano']; ?></title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+      <link rel="stylesheet" href="../../Assets/Css/Content.css" />
+      <link rel="stylesheet" href="../../Assets/Css/Footer.css" />
+    </head>
 
-<body>
+    <body>
 
-  <form action="mes.php" method="post">
-    <input type="hidden" name="statusMes" value="<?php echo $_SESSION['statusMes']; ?>">
-    <button class="btn btn-link">Voltar</button>
-  </form>
+      <form action="mes.php" method="post">
+        <input type="hidden" name="statusMes" value="<?php echo $_SESSION['statusMes']; ?>">
+        <button class="btn btn-link">Voltar</button>
+      </form>
 
-  <h1 class="display-5 fw-bold" style="text-align: center;">Despensas: gastos pessoais</h1>
-  <h3 style="text-align: center;">Quando estiver pronto, clique no botão de avançar para registrar as despensas da casa</h3>
+      <h1 class="display-5 fw-bold" style="text-align: center;">Despensas: gastos pessoais</h1>
+      <h3 style="text-align: center;">Quando estiver pronto, clique no botão de avançar para registrar as despensas da casa</h3>
 
-  <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="../../Assets/img/dia 15.png" alt="" width="72" height="70">
+      <div class="px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4" src="../../Assets/img/dia 15.png" alt="" width="72" height="70">
 
-    <h1 class="display-5 fw-bold">Entrada</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Por favor, digite um ano e mês válido na tela inicial.</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Nª</th>
-              <th scope="col">Descrição</th>
-              <th scope="col">valor</th>
-              <th scope="col">Data</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-
-            $contador = 1;
-            while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-            ?>
-              <tr>
-                <!-- <th scope="row">1</th> -->
-                <td> <?php echo $contador; ?> </td>
-                <td> <?php echo $linha['descricao']; ?> </td>
-                <td> <?php echo "R$" . $linha['valor']; ?> </td>
-                <td> <?php echo $linha['dataDespensa']; ?> </td>
-              </tr>
-            <?php
-              $contador++;
-            }
-
-            if ($adicionando_registro != null && $adicionando_registro == "REGISTRANDO ENTRADA") {
-            ?>
-              <form method="post">
-                <input type="hidden" name="adicionando_registro" value='SALVANDO REGISTRO'>
-                <input type="hidden" name="statusDespensa" value='3'>
+        <h1 class="display-5 fw-bold">Entrada</h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">Por favor, digite um ano e mês válido na tela inicial.</p>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <table class="table">
+              <thead>
                 <tr>
                   <th scope="col">Nª</th>
-                  <th scope="col">
-                    <input type="text" name="descricao">
-                  </th>
-                  <th scope="col">
-                    <input type="number" min="1" step="any" name="valor">
-                  </th>
-                  <th scope="col">
-                    <input type="date" name="data" value='<?php echo date("Y-m-d"); ?>'>
-                  </th>
-                  <button>Registrar</button>
+                  <th scope="col">Descrição</th>
+                  <th scope="col">valor</th>
+                  <th scope="col">Data</th>
                 </tr>
-              </form>
+              </thead>
+              <tbody>
+                <?php
+
+                $contador = 1;
+                while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+                ?>
+                  <tr>
+                    <!-- <th scope="row">1</th> -->
+                    <td> <?php echo $contador; ?> </td>
+                    <td> <?php echo $linha['descricao']; ?> </td>
+                    <td> <?php echo "R$" . $linha['valor']; ?> </td>
+                    <td> <?php echo $linha['dataDespensa']; ?> </td>
+                  </tr>
+                <?php
+                  $contador++;
+                }
+
+                if ($adicionando_registro != null && $adicionando_registro == "REGISTRANDO ENTRADA") {
+                ?>
+                  <form method="post">
+                    <input type="hidden" name="adicionando_registro" value='SALVANDO REGISTRO'>
+                    <input type="hidden" name="statusDespensa" value='3'>
+                    <tr>
+                      <th scope="col">Nª</th>
+                      <th scope="col">
+                        <input type="text" name="descricao">
+                      </th>
+                      <th scope="col">
+                        <input type="number" min="1" step="any" name="valor">
+                      </th>
+                      <th scope="col">
+                        <input type="date" name="data" value='<?php echo date("Y-m-d"); ?>'>
+                      </th>
+                      <button>Registrar</button>
+                    </tr>
+                  </form>
 
 
-            <?php
-            }
+                <?php
+                }
 
 
-            ?>
-            <!-- <tr>
+                ?>
+                <!-- <tr>
               <th scope="row">1</th>
               <td>Mark</td>
               <td>Otto</td>
@@ -224,84 +249,84 @@ and ( idStatus_despensa = 3 OR idstatus_despensa = 4 )          ");
               <td colspan="2">Larry the Bird</td>
               <td>@twitter</td>
             </tr> -->
-          </tbody>
+              </tbody>
 
-        </table>
+            </table>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="px-4 py-5 my-5 text-center">
+        <?php
+        if ($adicionando_registro == null) {
+        ?>
+          <form action="despensas.php" method="post">
+            <input type="hidden" value="REGISTRANDO ENTRADA" name="adicionando_registro">
+            <button>Adicionar um novo registro</button>
+          </form>
+        <?php
+        }
+
+        if ($adicionando_registro != null && $adicionando_registro == "REGISTRANDO ENTRADA") {
+        ?>
+          <tr>
+            <th scope="col">Nª</th>
+            <th scope="col">
+              <input type="text">
+            </th>
+            <th scope="col">
+              <input type="text">
+            </th>
+            <th scope="col">
+              <input type="text">
+            </th>
+            <button>Registrar</button>
+          </tr>
+
+        <?php
+        }
+        ?>
+
+
 
       </div>
-    </div>
-  </div>
-
-  <div class="px-4 py-5 my-5 text-center">
-    <?php
-    if ($adicionando_registro == null) {
-    ?>
-      <form action="despensas.php" method="post">
-        <input type="hidden" value="REGISTRANDO ENTRADA" name="adicionando_registro">
-        <button>Adicionar um novo registro</button>
-      </form>
-    <?php
-    }
-
-    if ($adicionando_registro != null && $adicionando_registro == "REGISTRANDO ENTRADA") {
-    ?>
-      <tr>
-        <th scope="col">Nª</th>
-        <th scope="col">
-          <input type="text">
-        </th>
-        <th scope="col">
-          <input type="text">
-        </th>
-        <th scope="col">
-          <input type="text">
-        </th>
-        <button>Registrar</button>
-      </tr>
-
-    <?php
-    }
-    ?>
-
-
-
-  </div>
 
 
 
 
-  <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="../../Assets/img/dia 15.png" alt="" width="72" height="70">
+      <div class="px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4" src="../../Assets/img/dia 15.png" alt="" width="72" height="70">
 
-    <h1 class="display-5 fw-bold">Saída</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Por favor, digite um ano e mês válido na tela inicial.</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Nª</th>
-              <th scope="col">Descrição</th>
-              <th scope="col">valor</th>
-              <th scope="col">Data</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
+        <h1 class="display-5 fw-bold">Saída</h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">Por favor, digite um ano e mês válido na tela inicial.</p>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Nª</th>
+                  <th scope="col">Descrição</th>
+                  <th scope="col">valor</th>
+                  <th scope="col">Data</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
 
-            while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-            ?>
-              <tr>
-                <th scope="row">1</th>
-                <td> <?php echo $linha['nome']; ?> </td>
-                <td> <?php echo $linha['email']; ?> </td>
-              </tr>
-            <?php
-            }
+                while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+                ?>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td> <?php echo $linha['nome']; ?> </td>
+                    <td> <?php echo $linha['email']; ?> </td>
+                  </tr>
+                <?php
+                }
 
 
-            ?>
-            <!-- <tr>
+                ?>
+                <!-- <tr>
               <th scope="row">1</th>
               <td>Mark</td>
               <td>Otto</td>
@@ -318,8 +343,8 @@ and ( idStatus_despensa = 3 OR idstatus_despensa = 4 )          ");
               <td colspan="2">Larry the Bird</td>
               <td>@twitter</td>
             </tr> -->
-          </tbody>
-        </table>
+              </tbody>
+            </table>
 
 
 
@@ -329,21 +354,21 @@ and ( idStatus_despensa = 3 OR idstatus_despensa = 4 )          ");
 
 
 
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
 
-  <div class="px-4 py-5 my-5 text-center">
-    <?php
-    if ($adicionando_registro == null) {
-    ?>
-      <form action="despensas.php" method="post">
-        <input type="hidden" value="REGISTRANDO ENTRADA" name="adicionando_registro">
-        <button>Adicionar um novo registro</button>
-      </form>
-    <?php
-    }
-    ?>
+      <div class="px-4 py-5 my-5 text-center">
+        <?php
+        if ($adicionando_registro == null) {
+        ?>
+          <form action="despensas.php" method="post">
+            <input type="hidden" value="REGISTRANDO ENTRADA" name="adicionando_registro">
+            <button>Adicionar um novo registro</button>
+          </form>
+        <?php
+        }
+        ?>
 
 
 
@@ -351,7 +376,7 @@ and ( idStatus_despensa = 3 OR idstatus_despensa = 4 )          ");
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
 
-</html>
+    </html>
