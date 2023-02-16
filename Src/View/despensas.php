38ff241a7e8abc9815a6e331ca1956e84423b14a
TiagoUniverse <tiagocesar68@gmail.com>
2023-02-16 15:47:42 -0300

    Teste de modal

--- Src/View/despensas.php
+++ Src/View/despensas.php
@@ -231,37 +231,8 @@ and ( idStatus_despensa = 3 OR idstatus_despensa = 4 )  Order By dataDespensa   
                       </div>
 
                       </td>
-                    <td>
-
-                      <!-- Button trigger modal -->
-                      <a type="button" data-bs-target="#exampleModal" data-bs-toggle="modal" >imitação</a>
-                      <button data-bs-toggle="modal" data-bs-target="#exampleModal"> oei</button>
-                      <button type="button" class="btn btn-primary"  data-bs-target="#exampleModal">
-                        Launch demo modal
-                      </button>
-
-                      <!-- Modal -->
-                      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                              ...
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                              <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-
-
-
-                    </td>
+                   
+                      
                   </tr>
                 <?php
                   $contador++;
