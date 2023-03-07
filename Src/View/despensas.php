92e322c8001a95de6d62eca76d7955491ecc4956
TiagoUniverse <tiagocesar68@gmail.com>
2023-03-07 16:07:52 -0300

    Conclusão da formatação do código de despensa

--- Src/View/despensas.php
+++ Src/View/despensas.php
@@ -260,7 +260,289 @@ if ($_SESSION['tipo_registro'] == "Registros da casa") {
 
   $consulta_Saida = $pdo->query("Select id, descricao, valor, DATE_FORMAT(dataDespensa, '%d/%m/%Y') as dataDespensa, ano, quinzena from despensas where status = 'ATIVO' 
   and ano = '{$_SESSION['ano']}'  and IdStatus_mes = '{$_SESSION['statusMes']}' and quinzena = '{$_SESSION['quinzena']}' 
-  and ( idstatus_despensa = 4 )  Order By month(dataDespensa)        "); 
+  and ( idstatus_despensa = 4 )  Order By month(dataDespensa)        ");
 }
 
 ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> Despensas de <?php echo $_SESSION['nomeMes']; ?> de <?php echo $_SESSION['ano']; ?></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../Assets/Css/Content.css" />
+  <link rel="stylesheet" href="../../Assets/Css/Footer.css" />
+</head>
+
+<body>
+
+  <form action="mes.php" method="post">
+    <input type="hidden" name="statusMes" value="<?php echo $_SESSION['statusMes']; ?>">
+    <button class="btn btn-link">Voltar</button>
+  </form>
+
+  <h1 class="display-5 fw-bold" style="text-align: center;"> <?php echo $_SESSION['quinzena'];  ?> </h1>
+  <h2 class="display-5 fw-bold" style="text-align: center;">Despensas: gastos pessoais</h2>
+  <h3 style="text-align: center;">Quando estiver pronto, clique no botão de avançar para registrar as despensas da casa</h3>
+
+  <div class="px-4 py-5 my-5 text-center">
+
+    <?php
+    if ($_SESSION['quinzena'] == "Quinzena 1") {
+      echo "<img class='d-block mx-auto mb-4' src='../../Assets/img/dia 15.png' alt='' width='72' height='70'> ";
+    } else {
+      echo "<img class='d-block mx-auto mb-4' src='../../Assets/img/dia 30.png' alt='' width='72' height='70'> ";
+    }
+    ?>
+
+    <h1 class="display-5 fw-bold" id="Entrada_title">Entrada</h1>
+    <p class="lead mb-4">Por favor, digite um ano e mês válido na tela inicial.</p>
+    <a href="#Saida_title"> Navegar até os registros de Saída</a>
+    <br><br>
+
+    <div class="col-lg-6 mx-auto" style="background-color:#c79797">
+
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Nª</th>
+              <th scope="col">Descrição</th>
+              <th scope="col">valor</th>
+              <th scope="col">Data</th>
+              <th scope="col">Alteração</th>
+              <th scope="col">Exclusão</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+
+            $contador = 1;
+            while ($linha = $consulta_Entrada->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+              <tr>
+                <!-- <th scope="row">1</th> -->
+                <td> <?php echo $contador; ?> </td>
+                <td> <?php echo $linha['descricao']; ?> </td>
+                <td> <?php echo "R$" . $linha['valor']; ?> </td>
+                <td> <?php echo $linha['dataDespensa']; ?> </td>
+                <td>
+
+                  <form action="alterar.php" method="post">
+                    <input type="hidden" value=" <?php echo $linha['id']; ?>" name="id">
+                    <input type="hidden" value="DESPENSAS" name="pagina_inicial">
+                    <button type="submit"> <img src="../../Assets//Icons//pencil.png" class="icon_exclusao"></button>
+                  </form>
+
+                </td>
+                <td>
+
+                  <form action="excluir.php" method="post">
+                    <input type="hidden" value=" <?php echo $linha['id']; ?>" name="id">
+                    <input type="hidden" value="DESPENSAS" name="pagina_inicial">
+                    <button type="submit"> <img src="../../Assets//Icons//x-mark-xxl.png" class="icon_exclusao"></button>
+                  </form>
+
+                </td>
+
+              </tr>
+            <?php
+              $contador++;
+            }
+
+            if ($adicionando_registro != null && $adicionando_registro == "REGISTRANDO ENTRADA") {
+            ?>
+              <form method="post">
+                <input type="hidden" name="adicionando_registro" value='SALVANDO REGISTRO ENTRADA'>
+                <input type="hidden" name="statusDespensa" value='3'>
+                <tr>
+                  <th scope="col">Nª</th>
+                  <th scope="col">
+                    <input type="text" name="descricao">
+                  </th>
+                  <th scope="col">
+                    <input type="number" min="1" step="any" name="valor">
+                  </th>
+                  <th scope="col">
+                    <input type="date" name="data" value='<?php echo date("Y-m-d"); ?>'>
+                  </th>
+
+                </tr>
+
+              <?php
+            }
+
+              ?>
+          </tbody>
+
+        </table>
+
+      </div>
+      <?php
+      if ($adicionando_registro != null && $adicionando_registro == "REGISTRANDO ENTRADA") {
+      ?>
+
+        <div class="row g-0 text-center">
+          <div class="col-sm-6 col-md-6">
+            <button type="submit" class="btn btn-primary">Registrar</button>
+            </form>
+          </div>
+          <div class="col-6 col-md-6">
+            <form action="despensas.php" method="post">
+              <input type="hidden" value="" name="adicionando_registro">
+              <button type="submit" class="btn btn-secondary">Cancelar</button>
+            </form>
+          </div>
+        </div>
+
+
+      <?php
+      }
+
+      if ($adicionando_registro == null) {
+      ?>
+        <form action="despensas.php" method="post">
+          <input type="hidden" value="REGISTRANDO ENTRADA" name="adicionando_registro">
+          <button type="submit" class="btn btn-primary">Adicionar um novo registro</button>
+        </form>
+      <?php
+      }
+      ?>
+
+    </div>
+  </div>
+
+
+
+  <div class="px-4 py-5 my-5 text-center">
+    <?php
+    if ($_SESSION['quinzena'] == "Quinzena 1") {
+      echo "<img class='d-block mx-auto mb-4' src='../../Assets/img/dia 15.png' alt='' width='72' height='70'> ";
+    } else {
+      echo "<img class='d-block mx-auto mb-4' src='../../Assets/img/dia 30.png' alt='' width='72' height='70'> ";
+    }
+    ?>
+
+    <h1 class="display-5 fw-bold" id="Saida_title">Saída</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Por favor, digite um ano e mês válido na tela inicial.</p>
+      <a href="#Entrada_title"> Navegar até os registros de Entrada</a>
+      <br><br>
+
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center" style="background-color:#c79797">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Nª</th>
+              <th scope="col">Descrição</th>
+              <th scope="col">valor</th>
+              <th scope="col">Data</th>
+              <th scope="col">Alteração</th>
+              <th scope="col">Exclusão</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+
+            $contador = 1;
+            while ($linha = $consulta_Saida->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+              <tr>
+                <!-- <th scope="row">1</th> -->
+                <td> <?php echo $contador; ?> </td>
+                <td> <?php echo $linha['descricao']; ?> </td>
+                <td> <?php echo "R$" . $linha['valor']; ?> </td>
+                <td> <?php echo $linha['dataDespensa']; ?> </td>
+                <td>
+
+                  <form action="alterar.php" method="post">
+                    <input type="hidden" value=" <?php echo $linha['id']; ?>" name="id">
+                    <input type="hidden" value="DESPENSAS" name="pagina_inicial">
+                    <button type="submit"> <img src="../../Assets//Icons//pencil.png" class="icon_exclusao"></button>
+                  </form>
+
+                </td>
+                <td>
+
+                  <form action="excluir.php" method="post">
+                    <input type="hidden" value=" <?php echo $linha['id']; ?>" name="id">
+                    <input type="hidden" value="DESPENSAS" name="pagina_inicial">
+                    <button type="submit"> <img src="../../Assets//Icons//x-mark-xxl.png" class="icon_exclusao"></button>
+                  </form>
+
+                </td>
+              </tr>
+            <?php
+              $contador++;
+            }
+
+            if ($adicionando_registro != null && $adicionando_registro == "REGISTRANDO SAIDA") {
+            ?>
+              <form method="post">
+                <input type="hidden" name="adicionando_registro" value='SALVANDO REGISTRO SAIDA'>
+                <input type="hidden" name="statusDespensa" value='3'>
+                <tr>
+                  <th scope="col">Nª</th>
+                  <th scope="col">
+                    <input type="text" name="descricao">
+                  </th>
+                  <th scope="col">
+                    <input type="number" min="1" step="any" name="valor">
+                  </th>
+                  <th scope="col">
+                    <input type="date" name="data" value='<?php echo date("Y-m-d"); ?>'>
+                  </th>
+
+                </tr>
+
+
+              <?php
+            }
+
+              ?>
+          </tbody>
+
+        </table>
+
+      </div>
+      <?php
+      if ($adicionando_registro != null && $adicionando_registro == "REGISTRANDO SAIDA") {
+      ?>
+
+        <div class="row g-0 text-center">
+          <div class="col-sm-6 col-md-6">
+            <button type="submit" class="btn btn-primary">Registrar</button>
+            </form>
+          </div>
+          <div class="col-6 col-md-6">
+            <form action="despensas.php" method="post">
+              <input type="hidden" value="" name="adicionando_registro">
+              <button type="submit" class="btn btn-secondary">Cancelar</button>
+            </form>
+          </div>
+        </div>
+
+
+      <?php
+      }
+
+      if ($adicionando_registro == null) {
+      ?>
+        <form action="despensas.php" method="post">
+          <input type="hidden" value="REGISTRANDO SAIDA" name="adicionando_registro">
+          <button type="submit" class="btn btn-primary">Adicionar um novo registro</button>
+        </form>
+      <?php
+      }
+      ?>
+
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
