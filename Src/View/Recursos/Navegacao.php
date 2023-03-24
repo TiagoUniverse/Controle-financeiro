44560ad0dbc98d82ae0b0a2d937eda9cd73e0872
TiagoUniverse <tiagocesar68@gmail.com>
2023-03-24 16:56:51 -0300

    Configurando a NavBar

--- Src/View/Recursos/Navegacao.php
+++ Src/View/Recursos/Navegacao.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 function talogado()
 {
   if ($_SESSION['connected'] !== 1) {
@@ -9,134 +7,61 @@ function talogado()
   }
 }
 
-
 talogado();
 
-?>
+/**
+ * Tipo de registro
+ * Fucnionamento: Os tipos de registros se classificam em registros pessoais e em registros da casa. Cada um deles possui poupança e despensas
+ * Data: 07/03/23
+ */
 
+if (!isset($_SESSION['tipo_registro'])) {
+  $_SESSION['tipo_registro'] = "Registros pessoais";
+}
 
-<!-- Header -->
-<div class="container">
-  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="home.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32">
-        <use xlink:href="#bootstrap"></use>
-      </svg>
-
-      <?php
-      if (isset($_SESSION['nomeMes'])  && isset($_SESSION['ano'])) {
-      ?>
-        <span class="fs-4">Controle monetário: <b> <?php echo $_SESSION['nomeMes']; ?> de <?php echo $_SESSION['ano']; ?> </b> </span>
-      <?php
-      } else {
-      ?>
-        <span class="fs-4">Controle monetário</span>
-      <?php
-      }
-      ?>
-    </a>
-
-    <?php
-    /**
-     * Tipo de registro
-     * Fucnionamento: Os tipos de registros se classificam em registros pessoais e em registros da casa. Cada um deles possui poupança e despensas
-     * Data: 07/03/23
-     */
-
-    if (!isset($_SESSION['tipo_registro'])) {
-      $_SESSION['tipo_registro'] = "Registros pessoais";
-    }
-
-
-    if (isset($_POST['tipo_registro'])) {
-      $_SESSION['tipo_registro'] = $_POST['tipo_registro'];
-    }
-
-
-    ?>
-
-
-
-    <ul class="nav nav-pills">
-
-      <?php
-
-      if ($_SESSION['tipo_registro'] == "Registros pessoais") {
-      ?>
-
-        <li class="nav-item">
-
-          <form action="home.php" method="post">
-            <input type="hidden" name="tipo_registro" value="Registros pessoais">
-            <button type="submit" class="btn btn-success">Registros pessoais</button>
-          </form>
-        </li>
-
-
-        <li class="nav-item">
-
-          <form action="home.php" method="post">
-            <input type="hidden" name="tipo_registro" value="Registros da casa">
-            <button type="submit" class="btn btn-secondary" style="margin-right:0.5cm;">Registros da casa</button>
-          </form>
-        </li>
-
-      <?php
-      } else {
-      ?>
-
-        <li class="nav-item">
-
-          <form action="home.php" method="post">
-            <input type="hidden" name="tipo_registro" value="Registros pessoais">
-            <button type="submit" class="btn btn-secondary">Registros pessoais</button>
-          </form>
-        </li>
-
-
-        <li class="nav-item">
-
-          <form action="home.php" method="post">
-            <input type="hidden" name="tipo_registro" value="Registros da casa">
-            <button type="submit" class="btn btn-success" style="margin-right:0.5cm;">Registros da casa</button>
-          </form>
-        </li>
-
-      <?php
-      }
-      ?>
-
-
-      <div class="dropdown text-end">
-        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-        </a>
-        <ul class="dropdown-menu text-small">
-
-          <?php
-          $nome_dividido = explode(" ", $_SESSION['user_name']);
-          ?>
-
-          <li><a class="dropdown-item" href="#"><?php echo "Perfil: <b>" . $nome_dividido[0] . "</b>"; ?> </a></li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-          <li><a class="dropdown-item" href="trocar-senha.php">Trocar senha</a></li>
-          <li><a class="dropdown-item" href="logoff.php">Sair</a></li>
-        </ul>
-      </div>
-    </ul>
-  </header>
-</div>
-<?php
-if ((isset($_POST['nomeMes']) && $_POST['ano']) || (isset($_POST['nomeMes']) && $_POST['ano'] == "")) {
-?>
-  <a href="home.php" class="botao-voltar">Voltar</a>
-<?php
+if (isset($_POST['tipo_registro'])) {
+  $_SESSION['tipo_registro'] = $_POST['tipo_registro'];
 }
 ?>
 
-
-<footer>
-  Tiago Universe, PE 2023.
-</footer>
+<header>
+  <h1>Controle monetário</h1>
+</header>
+<div class="nav-bar">
+  <?php
+  if ($_SESSION['tipo_registro'] == "Registros pessoais") {
+  ?>
+    <form action="home.php" method="post">
+      <input type="hidden" name="tipo_registro" value="Registros pessoais">
+      <button type="submit" class="nav-bar-selecionado">Registros pessoais</button>
+    </form>
+    <form action="home.php" method="post">
+      <input type="hidden" name="tipo_registro" value="Registros da casa">
+      <button type="submit" class="nav-bar-desmarcado">Registros da casa</button>
+    </form>
+  <?php
+  } else {
+  ?>
+    <form action="home.php" method="post">
+      <input type="hidden" name="tipo_registro" value="Registros pessoais">
+      <button type="submit" class="nav-bar-desmarcado">Registros pessoais</button>
+    </form>
+    <form action="home.php" method="post">
+      <input type="hidden" name="tipo_registro" value="Registros da casa">
+      <button type="submit" class="nav-bar-selecionado">Registros da casa</button>
+    </form>
+  <?php
+  }
+  ?>
+  <div class="dropdown">
+    <button onclick="myFunction()" class="dropbtn">
+      Configurações
+      <img src="../../Assets//Icons//dropdown.png">
+    </button>
+    <div id="myDropdown" class="dropdown-content">
+      <a href="#">Perfil: <b>Tiago</b></a>
+      <a href="#">Alterar senha</a>
+      <a href="#">Sair</a>
+    </div>
+  </div>
+</div>
--- Src/View/home.php
+++ Src/View/home.php
@@ -1,6 +1,6 @@
 <?php
 
- /**
+/**
  * ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
  * ║                                               CONTROLE FINANCEIRO                                                 ║
  * ║  ┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────┐  ║
@@ -13,238 +13,69 @@
  */
 
 require_once "conexao.php";
-require_once "Recursos/Navegacao.php";
-
 
 //Atualizando session
 $_SESSION['nomeMes'] = null;
 $_SESSION['statusMes'] = null;
 $_SESSION['quinzena'] = null;
 
-
-
 //Variáveis
-
-
-if (!isset($_POST['limpaFiltro'])){
+if (!isset($_POST['limpaFiltro'])) {
   $_SESSION['ano'] = null;
 }
 
-if (isset($_POST['limpaFiltro']) && $_POST['limpaFiltro'] == 1){
+if (isset($_POST['limpaFiltro']) && $_POST['limpaFiltro'] == 1) {
   $_SESSION['ano'] = null;
 } else if (isset($_POST['ano']) != null) {
   $_SESSION['ano'] = $_POST['ano'];
 }
-
 ?>
 
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="pt-br">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Home</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
-  integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../Assets/Css/Content.css" />
-  <link rel="stylesheet" href="../../Assets/Css/Footer.css" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="../../Assets//Css//styles.css" rel="stylesheet">
+  <link href="../../Assets//Css//dropdown.css" rel="stylesheet">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+  integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> -->
+  <title>Controle monetário</title>
 </head>
 
 <body>
-  <form method="POST" action="home.php">
-    <div class="input-group mb-3 filtro" >
-      <select class="form-control" name="ano">
-        <?php
-        for ($year = (int)date('Y'); 1900 <= $year; $year--) : ?>
-          <option value="<?= $year; ?>" n><?= $year; ?></option>
-        <?php endfor; ?>
-      </select>
-      <button type="submit" class="btn btn-primary">Pesquisar</button>
+  <?php require_once "Recursos/Navegacao.php"; ?>
+  <form method="POST" action="home.php" class="input-pesquisar">
+    <select name="ano" class="input-pesquisar-items">
+      <?php
+      for ($year = (int)date('Y'); 1900 <= $year; $year--) : ?>
+        <option value="<?= $year; ?>" n><?= $year; ?></option>
+      <?php endfor; ?>
+    </select>
+    <button type="submit" class="input-pesquisar-items">Pesquisar</button>
   </form>
-
-  <form method="POST" action="home.php">
-    <input type="hidden" name="limpaFiltro" value="1">
-    <button type="submit" class="btn btn-secondary" style="margin-left: 0.3cm;">Limpar resultado</button>
-  </form>
+  <div class="gallery">
+    <img src='../../Assets//img//1.png' alt="january">
+    <img src='../../Assets//img//february.png' alt="february">
+    <img src='../../Assets//img//march.png' alt="march">
+    <img src='../../Assets//img//april.png' alt="april">
+    <img src='../../Assets//img//may.png' alt="may">
+    <img src='../../Assets//img//june.png' alt="june">
+    <img src='../../Assets//img//july.png' alt="July">
+    <img src='../../Assets//img//august.png' alt="August">
+    <img src='../../Assets//img//september.png' alt="september">
+    <img src='../../Assets//img//october.png' alt="october">
+    <img src='../../Assets//img//november.png' alt="november">
+    <img src='../../Assets//img//december.png' alt="december">
+    <img src='../../Assets//img//savings.png' alt="savings">
   </div>
-
-  <?php
-  if ($_SESSION['ano'] != null) {
-  ?>
-    <div class="container px-4 text-center">
-      <h1><?php echo $_SESSION['ano']; ?> </h1>
-      <div class="row gx-5">
-
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Janeiro";
-            ?>
-            <input type="hidden" value="1" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Fevereiro";
-            ?>
-            <input type="hidden" value="2" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Março";
-            ?>
-            <input type="hidden" value="3" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Abril";
-            ?>
-            <input type="hidden" value="4" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Maio";
-            ?>
-            <input type="hidden" value="5" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Junho";
-            ?>
-            <input type="hidden" value="6" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Julho";
-            ?>
-            <input type="hidden" value="7" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Agosto";
-            ?>
-            <input type="hidden" value="8" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Setembro";
-            ?>
-            <input type="hidden" value="9" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Outubro";
-            ?>
-            <input type="hidden" value="10" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Novembro";
-            ?>
-            <input type="hidden" value="11" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box">
-          <form action="mes.php" method="post">
-            <?php
-            $NomeMes = "Dezembro";
-            ?>
-            <input type="hidden" value="12" name="statusMes">
-            <input type="hidden" value="<?php echo $NomeMes; ?>" name="nomeMes">
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo $NomeMes; ?></button>
-          </form>
-        </div>
-        <div class="box_Poupanca">
-          <form action="poupancas.php" method="post">
-            <?php
-            // $NomeMes = "Dezembro";
-            ?>
-            <input type="hidden" value="<?php echo $_SESSION['ano']; ?>" name="ano">
-            <button type="submit" class="btn btn-primary"><?php echo "Poupança de " . $_SESSION['ano']; ?></button>
-          </form>
-        </div>
-      </div>
-    </div>
-  <?php
-  } else {
-    ?>
-    <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="../../Assets/img/calendario.png" alt="" width="72" height="70">
-    
-    <h1 class="display-5 fw-bold">Busca vazia</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Digite um ano para começar</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      </div>
-    </div>
-  </div>
-    <?php
-  }
-  ?>
-
-
-  <script>
-
-  </script>
-
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  <footer>
+    Tiago Universe, PE 2023.
+  </footer>
+  <script src="../../Assets//Js//dropdown.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+  integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> -->
 </body>
 
 </html>
