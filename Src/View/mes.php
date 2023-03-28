7cb4e16ca980ebebb5c30b6be7c0ad209ac7a110
TiagoUniverse <tiagocesar68@gmail.com>
2023-03-28 17:56:23 -0300

    update error message

--- Src/View/mes.php
+++ Src/View/mes.php
@@ -44,35 +44,32 @@ if (isset($_POST['statusMes'])) {
   <?php
   if ($_SESSION['nomeMes'] == "" || $_SESSION['ano'] == "") {
   ?>
-    <div class="px-4 py-5 my-5 text-center">
-      <img class="d-block mx-auto mb-4" src="../../Assets/img/error.png" alt="" width="72" height="70">
-
-      <h1 class="display-5 fw-bold">Error de ano</h1>
-      <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">Por favor, digite um ano e mês válido na tela inicial.</p>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <a href="home.php" class="btn btn-primary btn-lg px-4 gap-3"> Voltar</a>
-        </div>
+    <main class='main-Mes'>
+      <div class="main-mes-itens">
+        <img src="../../Assets/img/error.png" alt="Error icon" width="42" height="40">
+        <h2>Error de consulta</h2>
+        <p>Por favor, informe um ano e mês válido na tela inicial e tente novamente.</p>
+        <button>Voltar ao menu</button>
       </div>
-    </div>
+    </main>
   <?php
   } else {
   ?>
     <main class='main-Mes'>
       <div class="main-mes-itens">
-      <img src="../../Assets/Icons/bank.png" alt="a pig bank" width="42" height="40" >
+        <img src="../../Assets/Icons/bank.png" alt="a pig bank" width="42" height="40">
         <h2><?php echo "Poupança de " . $_SESSION['ano'];  ?></h2>
         <p>Registre o dinheiro guardado nas economias.</p>
         <button>Registrar poupanças</button>
       </div>
       <div class="main-mes-itens">
-        <img src="../../Assets/img/dia 15.png" alt="a calendar with the number 15" width="42" height="40" >
+        <img src="../../Assets/img/dia 15.png" alt="a calendar with the number 15" width="42" height="40">
         <h2>Despensas: Primeira quinzena</h2>
         <p>Registre os gastos entre o dia 05 até o dia 19, que são os dias da entrada de salário da casa.</p>
         <button>Registrar despensas</button>
       </div>
       <div class="main-mes-itens">
-        <img src="../../Assets/img/dia 30.png" alt="a calendar with the number 30" width="42" height="40" >
+        <img src="../../Assets/img/dia 30.png" alt="a calendar with the number 30" width="42" height="40">
         <h2>Despensas: Segunda quinzena</h2>
         <p>Registre os gastos entre o dia 20 até dia 04 do próximo mês, antes de começar uma nova quinzena.</p>
         <button>Registrar despensas</button>
